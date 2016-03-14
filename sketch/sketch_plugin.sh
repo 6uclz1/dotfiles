f9187d035c54ce187fd021fc20b6497427a2edb5
6uclz1 <6uclz1@gmail.com>
2016-03-15 08:45:51 +0900

    minior changes

---
+++ sketch/sketch_plugin.sh
@@ -0,0 +1,13 @@
+cd ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/
+
+# Sketch Palettes
+git clone https://github.com/andrewfiorillo/sketch-palettes.git
+
+# SketchDistributor
+git clone https://github.com/PEZ/SketchDistributor.git
+
+# Sketch Use FontAwesome
+git clone https://github.com/keremciu/sketch-use-fontawesome.git
+
+# CSSketch
+git clone https://github.com/JohnCoates/CSSketch.git
