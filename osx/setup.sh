7121ba55366166f0a478a6e22989a9e539509a63
6uclz1 <6uclz1@gmail.com>
2016-03-16 17:28:10 +0900

    no message

--- osx/setup.sh
+++ osx/setup.sh
@@ -12,19 +12,138 @@ sudo pmset -a standbydelay 86400
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
 
-# sudo xcodebuild -license
+###############################################################################
+# General
+###############################################################################
 
-# xcode-select --install
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
 
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Avoid creating `.DS_Store` files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# brew doctor
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# brew update
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
 
-################################################
+# Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
+sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
+
+###############################################################################
+# Dock
+###############################################################################
+# Automatically hide or show the Dock
+defaults write com.apple.dock autohide -bool true
+
+###############################################################################
+# Finder
+###############################################################################
+
+# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Hide Desktop Icons Completely
+defaults write com.apple.finder CreateDesktop -bool false
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Set `${HOME}` as the default location for new Finder windows
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder : column view
+defaults write com.apple.finder FXPreferredViewStyle clmv
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Show the ~/Library directory
+chflags nohidden ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Show the hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+
+defaults write com.apple.spotlight orderedItems -array \
+	'{"enabled" = 1;"name" = "APPLICATIONS";}' \
+	'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+	'{"enabled" = 1;"name" = "DIRECTORIES";}' \
+	'{"enabled" = 1;"name" = "PDF";}' \
+	'{"enabled" = 1;"name" = "FONTS";}' \
+	'{"enabled" = 0;"name" = "DOCUMENTS";}' \
+	'{"enabled" = 0;"name" = "MESSAGES";}' \
+	'{"enabled" = 0;"name" = "CONTACT";}' \
+	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
+	'{"enabled" = 0;"name" = "IMAGES";}' \
+	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
+	'{"enabled" = 0;"name" = "MUSIC";}' \
+	'{"enabled" = 0;"name" = "MOVIES";}' \
+	'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+	'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+	'{"enabled" = 0;"name" = "SOURCE";}' \
+	'{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
+	'{"enabled" = 0;"name" = "MENU_OTHER";}' \
+	'{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
+	'{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
+	'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+	'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+
+	###############################################################################
+	# Address Book, Dashboard, iCal, TextEdit, and Disk Utility                   #
+	###############################################################################
+
+	# Enable the debug menu in Address Book
+	defaults write com.apple.addressbook ABShowDebugMenu -bool true
+
+	# Enable Dashboard dev mode (allows keeping widgets on the desktop)
+	defaults write com.apple.dashboard devmode -bool true
+
+	# Enable the debug menu in iCal (pre-10.8)
+	defaults write com.apple.iCal IncludeDebugMenu -bool true
+
+	# Use plain text mode for new TextEdit documents
+	defaults write com.apple.TextEdit RichText -int 0
+	# Open and save files as UTF-8 in TextEdit
+	defaults write com.apple.TextEdit PlainTextEncoding -int 4
+	defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+	# Enable the debug menu in Disk Utility
+	defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+	defaults write com.apple.DiskUtility advanced-image-options -bool true
+
+###############################################################################
+# Photos                                                                      #
+###############################################################################
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+###############################################################################
 # Safari
-################################################
+###############################################################################
+
 # Enable the `Develop` menu and the `Web Inspector`
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
@@ -39,11 +158,21 @@ defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 # Add a context menu item for showing the `Web Inspector` in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
+###############################################################################
+# Screen                                                                      #
+###############################################################################
 
-brew install rcmdnk/file/brew-file
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-export HOMEBREW_BREWFILE=./dotfiles/Brewfile
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
 
-brew file install
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+	"Dock" "Finder" "Mail" "Messages" "Photos" "Safari" "Spectacle" \
+	"SystemUIServer" "Terminal" ; do
+	killall "${app}" &> /dev/null
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
